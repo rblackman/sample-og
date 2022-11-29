73e5f6b166d13a1954f681aaad85fcb9849e022e
Ryan Blackman <rblackman@mtd.org>
2022-11-29 14:24:13 -0600

    initial commit

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Create Next App</title>
+      <meta content="og:image" name="https://picsum.photos/1200/627.webp?grayscale&blur=2" />
+    </>
+  )
+}
